62d6ae4e7246e4431effc5ccff091bfc90568841
Abhijith <abhijithsp1@gmail.com>
2026-06-04 15:09:34 +0530

    Fix lint configuration in build.gradle.kts to use correct property names

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     }
 
     lint {
-        isCheckReleaseBuilds = false
-        isAbortOnError = false
+        checkReleaseBuilds = false
+        abortOnError = false
     }
 
     defaultConfig {
